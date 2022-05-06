2ba1752c05c1d0474c44bea37e2d9abbfef82818
mrusb <mrusboy@mail.ru>
2022-05-07 02:07:21 +0300

    Rewriting Code on Kotlin

---
+++ app/src/main/java/ru/alexsas/mywardrobe_kt/screens/splash/SplashFragment.kt
@@ -0,0 +1,50 @@
+package ru.alexsas.mywardrobe_kt.screens.splash
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import ru.alexsas.mywardrobe_kt.R
+import ru.alexsas.mywardrobe_kt.databinding.FragmentSplashBinding
+import ru.alexsas.mywardrobe_kt.screens.main.MainActivity
+
+
+
+class SplashFragment : Fragment(R.layout.fragment_splash) {
+
+    private lateinit var binding: FragmentSplashBinding
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentSplashBinding.bind(view)
+
+        // just some animations example
+        renderAnimations()
+        launchMainScreen();
+    }
+
+    private fun launchMainScreen() {
+        val intent = Intent(requireContext(), MainActivity::class.java)
+
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+
+        startActivity(intent)
+    }
+
+    private fun renderAnimations() {
+        binding.loadingIndicator.alpha = 0f
+        binding.loadingIndicator.animate()
+            .alpha(0.7f)
+            .setDuration(1000)
+            .start()
+
+        binding.pleaseWaitTextView.alpha = 0f
+        binding.pleaseWaitTextView.animate()
+            .alpha(1f)
+            .setStartDelay(500)
+            .setDuration(1000)
+            .start()
+
+    }
+}
