773145e257720a3407800e350e4510d307e696f7
Alexandr Pravdin <alex.pravdinn@gmail.com>
2022-05-26 15:27:19 +0300

    Сломанный код, пишет оут оф баундс

--- app/src/main/java/ru/alexsas/mywardrobe_kt/screens/main/tabs/creating_outfit/OutfitFragment.kt
+++ app/src/main/java/ru/alexsas/mywardrobe_kt/screens/main/tabs/creating_outfit/OutfitFragment.kt
@@ -2,6 +2,7 @@ package ru.alexsas.mywardrobe_kt.screens.main.tabs.creating_outfit
 
 import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.core.graphics.blue
 import androidx.core.graphics.green
@@ -15,6 +16,7 @@ import com.google.firebase.ktx.Firebase
 import ru.alexsas.mywardrobe_kt.R
 import ru.alexsas.mywardrobe_kt.databinding.FragmentOutfitBinding
 import ru.alexsas.mywardrobe_kt.model.Item
+import kotlin.math.abs
 import kotlin.random.Random
 import ru.alexsas.mywardrobe_kt.utils.MatchingColorUtil as MatchingColorUtil1
 
@@ -27,14 +29,14 @@ class OutfitFragment : Fragment(R.layout.fragment_outfit) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         val randMax = 180
         val randMin = 0
-        var r: Int
-        var g: Int
-        var b: Int
         var switchOn = true
 
         var weardrobeSize = 0
-
+        //Создание массивов, хранящих цвета
         var myList: List<Item>
+        val shirtList: MutableList<Int> = mutableListOf()
+        val pantsList: MutableList<Int> = mutableListOf()
+        val bootsList: MutableList<Int> = mutableListOf()
         firestore = Firebase.firestore
         auth = FirebaseAuth.getInstance()
         userRef = firestore.collection("users").document(auth.uid.toString())/*
@@ -47,6 +49,8 @@ class OutfitFragment : Fragment(R.layout.fragment_outfit) {
 
         super.onViewCreated(view, savedInstanceState)
         mBinding = FragmentOutfitBinding.bind(view)
+
+        //Детект сдвига свича
         mBinding.Switcher.setOnCheckedChangeListener { compoundButton, isChecked ->
             switchOn = isChecked
         }
@@ -55,24 +59,42 @@ class OutfitFragment : Fragment(R.layout.fragment_outfit) {
             val itemsQuery = userRef.collection("clothes")
             itemsQuery.get().addOnSuccessListener { querySnapshot ->
                 myList = querySnapshot.toObjects(Item::class.java)
-
-                val Hex = Integer.toString(myList[Random.nextInt(0,myList.size)].color- 16777216, 16)
+                //перезапихивание предметов по 3 массивам
+                for (i in 0..myList.size) {
+                    Log.d("rar", "for cycle $i")
+                    when (myList[i].type) {
+                        "T-shirt" -> shirtList.add(myList[i].color)
+                        "Pants" -> pantsList.add(myList[i].color)
+                        "Boots" -> bootsList.add(myList[i].color)
+                    }
+                }
+                //взятие случайной футболки и получение ее цвета
+                val rand = Random.nextInt(0, shirtList.size - 1)
+                val Hex = Integer.toString(shirtList[rand] - 16777216, 16)
                 val color: Int = Color.parseColor("#$Hex")
-                r = color.red
-                g = color.green
-                b = color.blue
+
+                //Запуски функции в зависимости от свитча
                 if (switchOn) {
-                    matchTwoColorPressed(color)
+                    matchTwoColorPressed(color, pantsList)
                 } else {
-                    matchTreeColorPressed(color)
+                    matchTreeColorPressed(color, pantsList, bootsList)
                 }
             }
         }
     }
 
-    private fun matchTwoColorPressed(color: Int){
+
+    //Функция сочетания двух цветов
+    private fun matchTwoColorPressed(color: Int,pantsList: List<Int>){
         val Coloring = MatchingColorUtil1(color)
         val rgb = Coloring.twoColors()
+        /*var flag = false
+        for (i in 0..pantsList.size){
+            if (sameColors(rgb[1],pantsList[i])){
+                rgb[1] = pantsList[i]
+                flag = true
+            }
+        }*/
         //Футболка
         mBinding.ShirtView.setColorFilter(rgb[0])
         mBinding.ShirtBandView.setColorFilter(minColor(rgb[0]))
@@ -110,7 +132,9 @@ class OutfitFragment : Fragment(R.layout.fragment_outfit) {
         }
     }
 
-    private fun matchTreeColorPressed(color: Int){
+
+    //Функция сочетания трех цветов
+    private fun matchTreeColorPressed(color: Int,pantsList: List<Int>,bootsList: List<Int>){
         val colorUtil = MatchingColorUtil1(color)
         val rgb = colorUtil.threeColors()
         //mBinding.matchColorButton2.setBackgroundColor(Color.rgb(r,g,b))
@@ -153,33 +177,19 @@ class OutfitFragment : Fragment(R.layout.fragment_outfit) {
         }
     }
 
-    /*private fun fromRGBtoHUE(_r:Int,_g:Int,_b:Int) : Double {
-        val r = _r/255
-        val g = _g/255
-        val b = _b/255
-        val maximum = max(r,max(g,b))
-        val minimum = min(r,min(g,b))
-        if (maximum == r){
-            return 60.0*(g-b)/(maximum-minimum)
-        }
-        else if(maximum == g){
-            return (2.0 + (b-r)/(maximum-minimum))*60
-        }
-        else{
-            return (4.0 + (r-g)/(maximum-minimum))*60
-        }
-    }
 
-    private fun nearColors(Color1: Color, Color2: Color) : Boolean{
-        val r1 = Color1.red()
-        val g1 = Color1.green()
-        val b1 = Color1.blue()
-        val r2 = Color2.red()
-        val g2 = Color2.green()
-        val b2 = Color2.blue()
+    //Проверка на "похожесть" цвета
+    private fun sameColors(Color1: Int, Color2: Int) : Boolean{
+        val r1 = Color1.red
+        val g1 = Color1.green
+        val b1 = Color1.blue
+        val r2 = Color2.red
+        val g2 = Color2.green
+        val b2 = Color2.blue
         return (abs(r1-r2) < 30 && abs(g1-g2) < 30 && abs(b1 - b2) < 30)
     }
-    */
+
+    //Нахождение тона цвета потемнее
     fun minColor(color:Int):Int{ //Понижение цвета
         var r = color.red
         var g = color.green
@@ -192,8 +202,6 @@ class OutfitFragment : Fragment(R.layout.fragment_outfit) {
         else b = 0
         return Color.rgb(r,g,b)
     }
-
-
 
 }
 
